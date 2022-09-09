535c3a2cd86677a6e2deeb56210b159bd90cee48
Raphael Lim <raphael@Raphaels-MacBook-Pro.local>
2022-09-09 21:45:46 +0800

    feat: contact page and skills section done

--- pages/index.vue
+++ pages/index.vue
@@ -16,64 +16,84 @@
     </section>
     
     <section class="relative h-screen bg-stone-100 text-gray-900 flex items-center">
-      <div class="mx-auto container px-10 py-5 flex flex-col gap-5">
+      <div class="mx-auto container py-5 px-5 flex-col-reverse flex lg:flex-row items-center justify-between gap-10">
 
-        <div>
-          <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold">About me</h1>
-          <p class="text-lg md:text-xl lg:text-2xl break-normal">
-            I am an aspiring Software Devloper with the goal of creating quality digital products to help the world.
-          </p>
+        <div class="flex flex-col gap-5">
+          <div>
+            <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold">About me</h1>
+            <p class="text-lg md:text-xl lg:text-2xl">
+              I am an aspiring ✨Digital Artisan✨ with the goal of creating quality digital products to help the world.
+            </p>
+          </div>
+          
+          <div class="flex text-sm gap-x-3 gap-y-5 flex-wrap">
+            <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">Digital Artisan</h1>
+            <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">Temasek Polytechnic</h1>
+            <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">Computer Engineering</h1>
+            <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">GovTech Intern</h1>
+          </div>
         </div>
 
-        <div class="flex text-sm gap-x-3 gap-y-5 flex-wrap">
-          <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">Digital Artisan</h1>
-          <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">Temasek Polytechnic</h1>
-          <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">Computer Engineering</h1>
-          <h1 class="whitespace-nowrap bg-stone-300 text-stone-500 rounded-full text-center px-3 py-2">GovTech Intern</h1>
-        </div>
+        <img class="border rounded-full h-64 w-64 object-cover" src="/pfp.jpeg" alt="">
 
       </div>
     </section>
 
-      <section class="relative">
+      <section class="relative ">
         <div class="h-1/4 md:w-1/2 md:h-screen bg-stone-100 sticky top-0 flex items-center flex-col justify-center z-10 py-5">
             <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold text-center w-full">{{ this.skills[this.currentSection].title }}</h1>
             <h1 class="text-center text-xl w-full">{{ this.skills[this.currentSection].description }}</h1>
         </div>
         <div class="h-screen md:h-screen md:absolute md:top-0 bg-stone-300 flex w-full flex-wrap md:justify-end" id="mark1">
             <div class="grid grid-cols-2 gap-5 w-full md:grid-cols-3 h-1/2 place-self-center md:w-1/2 place-items-center">
-              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="/laravel.png" alt="Laravel">
-              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="/vue.png" alt="Vue">
-              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="/tailwind.png" alt="Tailwind">
-              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="/flutter.png" alt="flutter">
-              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="/nuxt.png" alt="nuxt">
-              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="/github.png" alt="github">
+              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="laravel.png" alt="Laravel">
+              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="vue.png" alt="Vue">
+              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="tailwind.png" alt="Tailwind">
+              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="flutter.png" alt="flutter">
+              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="nuxt.png" alt="nuxt">
+              <img class="h-24 bg-stone-100 rounded-md p-4 shadow-lg hover:scale-110 duration-200" src="github.png" alt="github">
             </div>
         </div>
         <div class="h-screen md:h-screen bg-stone-400 flex w-full items-center md:justify-end" id="mark2">
-            <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold text-center w-1/2">mark2</h1>
+            <div class="grid grid-cols-1 place-self-center md:w-1/2 place-items-center">
+              <div class="flex flex-col gap-2 w-3/4">
+                <a href="https://www.glance.sg">
+                  <img class="rounded-2xl shadow-lg hover:scale-105 duration-300" src="/glance.png" alt="glance" srcset="">
+                </a>
+                <h1 class="text-white text-lg text-center font-semibold">A platform to connect Companies or Organizations with Local Freelancers</h1>
+              </div>
+            </div>
         </div>
-           <div class="h-screen md:h-screen bg-stone-500 flex w-full items-center md:justify-end" id="mark3">
-            <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold text-center w-1/2">mark3</h1>
+         <div class="h-screen md:h-screen bg-stone-500 flex w-full items-center md:justify-end" id="mark2">
+            <div class="grid grid-cols-1 place-self-center md:w-1/2 place-items-center">
+              <div class="flex flex-col gap-2 w-3/4">
+                  <img class="rounded-2xl shadow-lg hover:scale-105 duration-300" src="/dean_list.jpeg" alt="dean list" srcset="">
+              </div>
+            </div>
         </div>
       </section>    
 
 
-      <section class="h-screen absolute bg-stone-100 w-full flex items-center justify-center flex-col gap-5 px-2">
-        <h1 class="text-2xl text-center">Let's make something <span class="font-bold">extravagant</span></h1>
-        <div class="flex flex-col">
-          <label for="">Name</label>
-          <input type="text">
+      <form class="h-screen absolute bg-stone-100 w-full flex items-center justify-center flex-col gap-5 px-2">
+        <h1 class="text-2xl md:text-4xl text-center">Let's make something <span class="font-bold">extravagant</span></h1>
+        <div class="flex flex-col w-1/2 gap-2">
+          <div>
+            <label for="">Name</label>
+            <input class="border border-stone-300 text-gray-900 text-sm rounded-lg focus:border-stone-500 outline-stone-500 block w-full p-2.5" type="text">
+          </div>
+
+          <div>
+            <label for="">Email</label>
+            <input class="border border-stone-300 text-gray-900 text-sm rounded-lg focus:border-stone-500 outline-stone-500 block w-full p-2.5" type="text">
+          </div>
+
+          <div>
+            <label for="">Message</label>
+            <textarea class="border border-stone-300 text-gray-900 text-sm rounded-lg focus:border-stone-500 outline-stone-500 block w-full p-2.5 md:h-64"/>
+          </div>
         </div>
-        <div class="flex flex-col ">
-          <label for="">Email</label>
-          <input type="text">
-        </div>
-        <div class="flex flex-col ">
-          <label for="">Message</label>
-          <textarea/>
-        </div>
-      </section>
+        <button class="bg-stone-500 px-4 py-2 rounded-xl text-white text-xl font-semibold shadow-lg hover:bg-stone-800 duration-200 hover:scale-110 active:scale-90">Collaborate</button>
+      </form>
 
   </div>
 </template>

b10e0c1892ec5e3d9e60695b44e972e04f5b8709
Raphael Lim <raphael@Raphaels-MacBook-Pro.local>
2022-09-22 09:48:44 +0800

    feat: set message response

--- server/api/reinhardt.reply.post.js
+++ server/api/reinhardt.reply.post.js
@@ -4,6 +4,15 @@ const config = useRuntimeConfig()
 
 
 export default defineEventHandler(async (event) => {
-    return 'message received'
+    const body = await useBody(event)
+    let message = `Message Recevied ${body}`
+    return axios.post(`https://api.telegram.org/bot${config.reinhardt}/sendMessage`, {
+        chat_id: config.chatId,
+        text: message
+    }).then((event) => {
+        return 'success'
+    }).catch(function (error) {
+        return error
+    });
 })
 

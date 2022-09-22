5c6616aa9fc992d34a59e2eb25638f811640dc36
Raphael Lim <raphael@Raphaels-MacBook-Pro.local>
2022-09-22 11:48:24 +0800

    fix: reinhardt Trying to get reinhardt to read 3

--- server/api/reinhardt/reply.post.js
+++ server/api/reinhardt/reply.post.js
@@ -5,7 +5,7 @@ const config = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
     const body = await useBody(event)
-    let message = `Message Recevied you said ${body.message}`
+    let message = `Message Recevied you said ${body.message.text}`
     return axios.post(`https://api.telegram.org/bot${config.reinhardt}/sendMessage`, {
         chat_id: config.chatId,
         text: message

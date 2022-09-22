14f2cdffffa41d42d2de1f16936ab388d5fce72f
Raphael Lim <raphael@Raphaels-MacBook-Pro.local>
2022-09-22 10:11:46 +0800

    fix: reinhardt can reply now

--- server/api/reinhardt/reply.post.js
+++ server/api/reinhardt/reply.post.js
@@ -6,7 +6,6 @@ const config = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
     const body = await useBody(event)
     let message = `Message Recevied ${body}`
-    return message
     return axios.post(`https://api.telegram.org/bot${config.reinhardt}/sendMessage`, {
         chat_id: config.chatId,
         text: message
